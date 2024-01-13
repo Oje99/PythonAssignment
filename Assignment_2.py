7fa126389f62da174701f5c9224d9bb334d89dbb
oje99 <oje99@hotmail.co.uk>
2024-01-13 10:31:06 +0000

    Added API URL and inital code for calling API

--- Assignment_2.py
+++ Assignment_2.py
@@ -3,4 +3,12 @@ import time
 
 API_REQUEST_DELAY = 1
 
+def get_api_fact():
+
+    api_url = "http://dog-api.kinduff.com/api/facts"
+
+    try:
+        response = requests.get(api_url)
+        response.raise_for_status()
+        dog_api_fact = response.json()
 
